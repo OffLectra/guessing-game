882372e3db55314d4aa3da4f2038800b7323e2e9
Ulia Skripka <minilektrina@gmail.com>
2023-02-23 03:00:09 +0300

    feat: task is solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,12 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.MinValue = min;
+        this.MaxValue = max;
     }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
+    guess() { return Math.round((this.MaxValue + this.MinValue)/2); }
+    lower() { this.MaxValue = Math.round((this.MaxValue + this.MinValue)/2); } 
+    greater() { this.MinValue = Math.round((this.MaxValue + this.MinValue)/2); } 
+} 
 
 module.exports = GuessingGame;
